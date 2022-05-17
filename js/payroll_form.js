32e15a81b38825428176bb88d5c251157fdcb247
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-05-17 22:38:32 +0530

    Added: Table for Employee Payroll App

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -70,7 +70,7 @@ const createEmployeePayroll = () => {
     employeePayrollData.department = getSelectedValues('[name=department]');
     employeePayrollData.salary = getInputValueById('#salary');
     employeePayrollData.note = getInputValueById('#notes');
-    employeePayrollData.id = new Date().getTime() + 1;
+    employeePayrollData.id = new Date().getTime() ;
     alert(employeePayrollData.toString());
     return employeePayrollData;
 }
